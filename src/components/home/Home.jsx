a5c1c2ce946ea0d06c064b0194cf792a345cc9e0
MRJAHID <mrjahid350@gmail.com>
2022-08-01 17:55:53 +0600

    Awards

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/Hero";
 import Featured from "./featured/Featured";
 import Recent from "./recent/Recent";
+import Award from "./award/Award";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Featured />
       <Recent />
+      <Award />
     </>
   );
 };
