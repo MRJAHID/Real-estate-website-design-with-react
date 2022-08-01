7afca2a229a257c6e2b4aa790d4d8143d4d1e063
MRJAHID <mrjahid350@gmail.com>
2022-08-01 14:23:21 +0600

    Header

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Pages = () => {
-  return <div></div>;
+  return (
+    <>
+      <Router>
+        <Header />
+        {/*<Route exact path="/about" component={About} />*/}
+        {/*<Route exact path="/about" component={About} />*/}
+        {/*<Route exact path="/about" component={About} />*/}
+        {/*<Route exact path="/about" component={About} />*/}
+        {/*<Route exact path="/about" component={About} />*/}
+      </Router>
+    </>
+  );
 };
 
 export default Pages;
