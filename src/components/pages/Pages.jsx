ab20cc92e7fc7b54cf5c4e706410d026462862f7
MRJAHID <mrjahid350@gmail.com>
2022-08-01 14:29:18 +0600

    Route Set

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import Header from "../common/header/Header";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../home/Home";
 
 const Pages = () => {
   return (
     <>
       <Router>
         <Header />
-        {/*<Route exact path="/about" component={About} />*/}
-        {/*<Route exact path="/about" component={About} />*/}
-        {/*<Route exact path="/about" component={About} />*/}
-        {/*<Route exact path="/about" component={About} />*/}
-        {/*<Route exact path="/about" component={About} />*/}
+        <Route exact path="/" component={<Home />} />
       </Router>
     </>
   );
